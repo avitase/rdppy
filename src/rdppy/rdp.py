36abd71f78ffd5e8a374d21d186b5204655c3bb7
Nis Meinert <nis.meinert@gmail.com>
2021-10-15 22:43:16 +0200

    Remove unnecessary list in max

--- src/rdppy/rdp.py
+++ src/rdppy/rdp.py
@@ -10,7 +10,7 @@ def dist2(points, start, end):
     max_p1 = np.dot(start - points, d).max()
     max_p2 = np.dot(points - end, d).max()
 
-    return max([max_p1, max_p2, 0]) ** 2 + np.cross(points - start, d) ** 2
+    return max(max_p1, max_p2, 0) ** 2 + np.cross(points - start, d) ** 2
 
 
 def _lossy_compress(points, threshold):
